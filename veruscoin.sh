3dcb700d33a267d5890676d9604283f261398556
eza02 <106399459+eza02@users.noreply.github.com>
2022-05-28 00:11:17 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,7 @@
+scrib hellminer veruscoin
+
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+
+tar xf hellminer_cpu_linux.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 8
